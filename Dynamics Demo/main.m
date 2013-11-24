2bc6dcc6282a6aa6a108080e9af5881a812f6a53
Simon Whitaker <sw@netcetera.org>
2013-11-24 19:03:35 +0000

    Initial Commit

---
+++ Dynamics Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dynamics Demo
+//
+//  Created by Simon Whitaker on 24/11/2013.
+//  Copyright (c) 2013 Simon Whitaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SWAppDelegate class]));
+    }
+}
