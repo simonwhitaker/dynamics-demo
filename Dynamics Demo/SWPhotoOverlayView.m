9e4a82a19e0dc436bba9e6c74181b9abb943813e
Simon Whitaker <sw@netcetera.org>
2013-11-24 20:53:48 +0000

    Add dynamics

--- Dynamics Demo/SWPhotoOverlayView.m
+++ Dynamics Demo/SWPhotoOverlayView.m
@@ -10,6 +10,8 @@
 
 @interface SWPhotoOverlayView()
 @property (nonatomic, readwrite, weak) UIImageView *imageView;
+@property (nonatomic, strong) UIDynamicAnimator *animator;
+@property (nonatomic, strong) UIAttachmentBehavior *attachmentBehavior;
 @end
 
 @implementation SWPhotoOverlayView
@@ -18,26 +20,81 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.85];
-        CGRect imageViewFrame = CGRectInset(self.bounds, 50, 50);
+        self.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.8];
+        CGRect imageViewFrame = CGRectInset(self.bounds, 50, 150);
         UIImageView *imageView = [[UIImageView alloc] initWithFrame:imageViewFrame];
-        imageView.backgroundColor = [UIColor clearColor];
-        imageView.contentMode = UIViewContentModeScaleAspectFit;
-        imageView.layer.shadowColor = [[UIColor blackColor] CGColor];
-        imageView.layer.shadowOffset = CGSizeMake(0, 2.0);
-        imageView.layer.shadowRadius = 4.0;
-        imageView.layer.shadowOpacity = 0.8;
+        imageView.backgroundColor = [UIColor redColor];
+        imageView.contentMode = UIViewContentModeScaleAspectFill;
+        imageView.clipsToBounds = YES;
         [self addSubview:imageView];
         self.imageView = imageView;
+        self.imageView.userInteractionEnabled = YES;
+        
+        self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self];
         
         UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapGesture:)];
         [self addGestureRecognizer:tapGestureRecognizer];
+        
+        UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
+        [self.imageView addGestureRecognizer:panGestureRecognizer];
     }
     return self;
 }
 
+- (void)handlePanGesture:(UIGestureRecognizer*)gestureRecognizer {
+    if (gestureRecognizer.state == UIGestureRecognizerStateBegan) {
+        CGPoint touchLocation = [gestureRecognizer locationInView:self.imageView];
+        UIOffset offset = UIOffsetMake(touchLocation.x - self.imageView.frame.size.width/2, touchLocation.y - self.imageView.frame.size.height/2);
+        UIAttachmentBehavior *attachment = [[UIAttachmentBehavior alloc] initWithItem:self.imageView offsetFromCenter:offset attachedToAnchor:[gestureRecognizer locationInView:self]];
+        attachment.length = 0.0;
+        [self.animator addBehavior:attachment];
+        self.attachmentBehavior = attachment;
+        
+        UIGravityBehavior *fall = [[UIGravityBehavior alloc] initWithItems:@[self.imageView]];
+        fall.magnitude = 4.0;
+        fall.action = ^{
+            if (!CGRectIntersectsRect(self.frame, self.imageView.frame)) {
+                self.animator = nil;
+                [self fadeOut:0.2];
+            }
+        };
+        [self.animator addBehavior:fall];
+    }
+    else if (gestureRecognizer.state == UIGestureRecognizerStateEnded || gestureRecognizer.state == UIGestureRecognizerStateCancelled) {
+        [self.animator removeBehavior:self.attachmentBehavior];
+        self.attachmentBehavior = nil;
+    }
+    else if (gestureRecognizer.state == UIGestureRecognizerStateChanged) {
+        self.attachmentBehavior.anchorPoint = [gestureRecognizer locationInView:self];
+    }
+}
+
 - (void)handleTapGesture:(UIGestureRecognizer*)gestureRecognizer {
-    [UIView animateWithDuration:0.5 animations:^{
+    if (CGRectContainsPoint(self.imageView.frame, [gestureRecognizer locationInView:self])) {
+        NSLog(@"Got a tap on the image view");
+        
+        UIPushBehavior *kick = [[UIPushBehavior alloc] initWithItems:@[self.imageView] mode:UIPushBehaviorModeInstantaneous];
+        kick.angle = M_PI_2 * 3;
+        kick.magnitude = 20.0;
+        
+        UIGravityBehavior *fall = [[UIGravityBehavior alloc] initWithItems:@[self.imageView]];
+        fall.magnitude = 4.0;
+        fall.action = ^{
+            if (!CGRectIntersectsRect(self.frame, self.imageView.frame)) {
+                self.animator = nil;
+                [self fadeOut:0.2];
+            }
+        };
+
+        [self.animator addBehavior:kick];
+        [self.animator addBehavior:fall];
+    } else {
+        [self fadeOut:0.5];
+    }
+}
+
+- (void)fadeOut:(NSTimeInterval)duration {
+    [UIView animateWithDuration:duration animations:^{
         self.alpha = 0.0;
     } completion:^(BOOL finished) {
         [self removeFromSuperview];
